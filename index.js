f5d3d8ceefc64ab590e620e2e466723ede691b07
张红亮 <zhanghongliang@zhanghoiangsAir.lan>
2019-08-04 19:33:35 +0800

    emoji

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+import { SCAVENGER_CLASSES } from "./scavenger_classes";
 import { loadFrozenModel, FrozenModel } from "@tensorflow/tfjs-converter";
 import "babel-polyfill";
 import * as tfc from "@tensorflow/tfjs-core";
 class Game {
+  errNum = 0;
   isRunning = true;
   MODEL_FILE_URL = "/model/tensorflowjs_model.pb";
   WEIGHT_MANIFEST_FILE_URL = "/model/weights_manifest.json";
+  currentEmoji = {
+    emoji: "📺",
+    name: "tv",
+    path: "/img/emojis/game/tv.svg"
+  };
   async load() {
     this.model = await loadFrozenModel(
       this.MODEL_FILE_URL,
@@ -12,7 +19,25 @@ class Game {
     );
   }
   emojiScavengerMobileNet = {
-    predict(input) {
+    getTopKClasses(predictions, topK) {
+      const values = predictions.dataSync();
+      predictions.dispose();
+
+      let predictionList = [];
+      for (let i = 0; i < values.length; i++) {
+        predictionList.push({ value: values[i], index: i });
+      }
+      predictionList = predictionList
+        .sort((a, b) => {
+          return b.value - a.value;
+        })
+        .slice(0, topK);
+
+      return predictionList.map(x => {
+        return { label: SCAVENGER_CLASSES[x.index], value: x.value };
+      });
+    },
+    predict(input, model) {
       const PREPROCESS_DIVISOR = tfc.scalar(255 / 2);
       const INPUT_NODE_NAME = "input";
       const OUTPUT_NODE_NAME = "final_result";
@@ -27,7 +52,7 @@ class Game {
       ]);
       const dict = {};
       dict[INPUT_NODE_NAME] = reshapedInput;
-      return this.model.execute(dict, OUTPUT_NODE_NAME);
+      return model.execute(dict, OUTPUT_NODE_NAME);
     }
   };
   /** Our MobileNet instance and how we get access to our trained model. */
@@ -61,7 +86,7 @@ class Game {
           [VIDEO_PIXELS, VIDEO_PIXELS, 3]
         );
 
-        return this.emojiScavengerMobileNet.predict(pixelsCropped);
+        return this.emojiScavengerMobileNet.predict(pixelsCropped, this.model);
       });
 
       // This call retrieves the topK matches from our MobileNet for the
@@ -94,7 +119,46 @@ class Game {
 
     // To ensure better page responsiveness we call our predict function via
     // requestAnimationFrame - see goo.gl/1d9cJa
-    requestAnimationFrame(() => this.predict());
+    if (this.errNum <= 100) {
+      requestAnimationFrame(() => this.predict());
+    } else {
+    }
+  }
+  /**
+   * Determines if our top 2 matches from the MobileNet is the emoji we are
+   * currently looking to find.
+   * @param emojiNameTop1 Top guess emoji name.
+   * @param emojiNameTop2 Second place guess emoji name.
+   */
+  checkEmojiMatch(emojiNameTop1, emojiNameTop2) {
+    // If our top guess is different from when we last checked update the
+    // top guess.
+    if (this.topItemGuess !== emojiNameTop1) {
+      this.topItemGuess = emojiNameTop1;
+
+      // As soon as we have a top guess available try to speak so the game
+      // and prediction feels snappy instead of waiting for the 2.5 second
+      // speak delay to speak out the initial guess.
+      if (this.firstSpeak) {
+        let msg = ui.sleuthSpeakingSeeingMsg;
+        ui.setSleuthSpeakerText(msg);
+        this.speak(msg);
+        this.firstSpeak = false;
+      }
+    }
+    this.errNum++;
+    if (
+      this.currentEmoji.name === emojiNameTop1 ||
+      this.currentEmoji.name === emojiNameTop2 ||
+      this.errNum === 100
+    ) {
+      this.emojiFound();
+    } else {
+      console.log(this.errNum);
+    }
+  }
+  emojiFound() {
+    console.log(this.currentEmoji);
   }
 }
 const game = new Game();
